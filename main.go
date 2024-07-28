cd736de97f59a2d52748d49f08569dfff0be1e51
bouaik <bouaik.lhou@gmail.com>
2024-07-28 01:51:06 +0100

    very simple  url shortner cli in go

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var (
+	source = rand.NewSource(time.Now().UnixNano())
+	rng    = rand.New(source)
+)
 
 func main() {
+	urls := make(map[string]string)
 
-	fmt.Println("hello, world!")
+	for {
+		fmt.Print("Enter your url : ")
+		in := bufio.NewReader(os.Stdin)
+		line, _ := in.ReadString('\n')
+
+		urls[generateShortKey()] = line
+
+		for k, v := range urls {
+			fmt.Printf("%s =====> %s", k, v)
+		}
+	}
+
+}
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 9
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(shortKey)
 }
